34ce4199134884696e711832eb48189e379e951d
furkanguvenme <furkanguven.me@gmail.com>
2024-07-22 13:18:42 +0300

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return clock < 8 || clock > 20;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
