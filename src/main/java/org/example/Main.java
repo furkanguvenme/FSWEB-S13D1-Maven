c688b778e4c264369dc03e20cb0dd4875c3df9f5
furkanguvenme <furkanguven.me@gmail.com>
2024-07-22 13:54:00 +0300

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isCatPlaying(true,10));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        return clock < 8 || clock > 20;
+        if(clock < 0 || clock > 23) {
+            return false;
+        }
+        if ((clock < 8 || clock >= 20) && isBarking) {
+            return true;
+        }
+        return false;
+
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
@@ -15,17 +23,24 @@ public class Main {
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
      if(isSummer){
-         return 25 < temp && temp < 45;
+         return 25 <= temp && temp <= 45;
      } else {
-         return 25 < temp && temp < 35;
+         return 25 <= temp && temp <= 35;
      }
     }
 
     public static double area(double width, double height) {
-       
+       if(width >= 0 && height >= 0) {
+           return height*width;
+       }
+       return -1;
+
     }
 
     public static double area(double radius) {
-       
+       if(radius >= 0) {
+           return radius*radius*Math.PI;
+       }
+       return -1;
     }
 }
